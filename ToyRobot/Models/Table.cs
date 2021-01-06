8fe5c1dd4be26245bcd6de437ccf87b258fe6f23
Luke Belton <Luke.Belton@mottmac.com>
2021-01-06 19:42:18 +0000

    adds Table

--- ToyRobot/Models/Table.cs
+++ ToyRobot/Models/Table.cs
@@ -4,5 +4,10 @@
     {
         public int XDimension { get; set; }
         public int YDimension { get; set; }
+
+        public bool ContainsCoordinate(int x, int y)
+        {
+            return (x >= 0 && y >= 0 && x < XDimension && y < YDimension);
+        }
     }
 }
