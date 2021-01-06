9b4912fa942ec7820679e9e20ae5966ddae4d59e
Luke Belton <Luke.Belton@mottmac.com>
2021-01-06 22:27:40 +0000

    updates Program to take commands from console input

--- ToyRobot/Program.cs
+++ ToyRobot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using ToyRobotSimulator.Helpers;
+using ToyRobotSimulator.Models;
 
 namespace ToyRobotSimulator
 {
@@ -6,7 +8,16 @@ namespace ToyRobotSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Toy Robot Simulator!");
+            var commandManager = new CommandManager<ToyRobot>();
+            var controller = new RobotController<ToyRobot>(new Table {XDimension = 5, YDimension = 5 });
+            var robot = new ToyRobot();
+            while (true)
+            {
+                var rawCommand = Console.ReadLine();
+                var command = commandManager.GetCommandFromInput(rawCommand);
+                robot = controller.ExecuteSingleCommand(command, robot);
+            }
         }
     }
 }
