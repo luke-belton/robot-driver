6c53ee1b3f732297d9730ad5721a3a7438123561
Luke Belton <Luke.Belton@mottmac.com>
2021-01-06 19:41:33 +0000

    adds RobotCommandException

---
+++ ToyRobot/Exceptions/RobotCommandException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ToyRobotSimulator.Exceptions
+{
+    public class RobotCommandException : Exception
+    {
+        public RobotCommandException(string message) : base(message) { }
+    }
+}
