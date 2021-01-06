98c29d1f76788bee1e27068b2e434d7b32e68d47
Luke Belton <Luke.Belton@mottmac.com>
2021-01-06 14:26:34 +0000

    adds Table and ToyRobot objects

---
+++ ToyRobot/Models/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace ToyRobotSimulator.Models
+{
+    public class Table
+    {
+        public int XDimension { get; set; }
+        public int YDimension { get; set; }
+    }
+}
---
+++ ToyRobot/Models/ToyRobot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ToyRobotSimulator.Constants;
+
+namespace ToyRobotSimulator.Models
+{
+    public interface IRobotReporter
+    {
+        public void ReportPosition();
+    }
+
+    public interface IRobotLocator
+    {
+        public bool IsPlaced { get; set; }
+
+        public RobotPosition Position { get; set; }
+
+        public CompassDirection Facing { get; set; }
+    }
+
+    public interface IRobot : IRobotLocator, IRobotReporter { }
+
+    public class ToyRobot : IRobot
+    {
+        public bool IsPlaced { get; set; } = false;
+
+        public RobotPosition Position { get; set; }
+
+        public CompassDirection Facing { get; set; }
+
+        public void ReportPosition() => Console.WriteLine(ToString());
+
+        public override string ToString()
+        {
+            return $"{Position.X},{Position.Y},{Facing.ToString().ToUpper()}";
+        }
+    }
+
+    public class RobotPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
